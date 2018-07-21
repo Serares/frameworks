98dd09babd36ab55b259ce16ce48d527e963b77f
rares <jianu.rares30@gmail.com>
2018-07-21 20:48:20 +0300

    aproape gata

---
+++ react-weather-app/weather/src/components/ziua-curenta.js
@@ -0,0 +1,32 @@
+
+
+import React from 'react';
+
+
+class ZiuaActuala extends React.Component{
+
+    render(){
+        const {icon,dataOra,description,temperatura,temperaturaMin,temperaturaMax} = this.props;
+        return(
+
+            <div className="ziua-curenta">
+            <div id="icon"><img alt="imagineZiCurenta" src={`http://openweathermap.org/img/w/${icon}.png`}/></div>
+            <div className="ora">
+                {dataOra}
+            </div>
+            <div id="descriere">{description}</div>
+            <span id="temp"> {temperatura} </span>
+            <span id="tempMin"> {temperaturaMin} </span>
+            <span id="tempMin"> {temperaturaMax} </span>
+
+            </div>
+
+
+        )
+    }
+
+}
+
+
+
+export default ZiuaActuala; 
---
+++ react-weather-app/weather/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from '../node_modules/react-dom';
+import App from './components/App';
+
+
+
+ReactDOM.render(<App/>, document.getElementById('root'));
