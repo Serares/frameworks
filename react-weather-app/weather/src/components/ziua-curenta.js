03e19c0adf14958835c26289f6c833b98e8d8bc8
Serares <jianu.rares30@gmail.com>
2018-08-15 02:14:01 +0300

    small

--- react-weather-app/weather/src/components/ziua-curenta.js
+++ react-weather-app/weather/src/components/ziua-curenta.js
@@ -12,11 +12,11 @@ class ZiuaActuala extends React.Component{
             <div className="ziua-curenta">
             <div id="numeOras">{nume}</div>
             <div id="icon"><img alt="imagineZiCurenta" src={`http://openweathermap.org/img/w/${icon}.png`}/></div>
-            <div className="ora">{dataOra}</div>
+            <div className="ora">Data {dataOra}</div>
             <div id="descriere">{description}</div>
-            <span id="temp"> {temperatura} </span>
-            <span id="tempMin"> {temperaturaMin} </span>
-            <span id="tempMin"> {temperaturaMax} </span>
+            <span id="temp"> Temp{temperatura} </span>
+            <span id="tempMin">Min {temperaturaMin} </span>
+            <span id="tempMin"> Max {temperaturaMax} </span>
 
             </div>
 
