22a2cbe5ffb7cc992588a4ec3ea159224cb504d8
Serares <jianu.rares30@gmail.com>
2018-08-18 20:05:01 +0300

    redux

---
+++ react-redux-app/src/components/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+const App = () => {
+    return(
+        <div className = 'container'>
+    <div className = 'row'>
+        Hello World
+    </div>
+    
+    </div>
+    )
+    
+
+}
+
+
+export default App
