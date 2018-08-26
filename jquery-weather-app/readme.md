44fa4d2fa80a327980575f10663dd13c22d32448
Serares <jianu.rares30@gmail.com>
2018-08-26 15:00:20 +0300

    sass

---
+++ jquery-weather-app/readme.md
@@ -0,0 +1,3 @@
+
+
+<p style="color:red;">Weather App Using webpack, jquery, sass</p> <br>
---
+++ jquery-weather-app/src/app/index.js
@@ -0,0 +1,11 @@
+import '../style/app.scss';
+
+
+class TestClass{
+    constructor(){
+        var msg = 'Using es5'
+        console.log(msg);
+    }
+}
+
+var test = new TestClass();
